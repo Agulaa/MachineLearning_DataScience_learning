24b5d14f56afb19c82a107e2fe255b4bcf08862e
Agulaa <agnieszkaxrusin@gmail.com>
2017-12-23 13:11:17 +0100

    Change visualization plot

--- ml.py
+++ ml.py
@@ -1,36 +1,73 @@
+import pandas as pd
+import numpy as np
 import random, nltk
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
-nltk.download('names') #lista angielski męskich i żeńskich imion
+
+nltk.download('names') #english list of name -> male nad female
 
 male_names = nltk.corpus.names.words('male.txt')
 female_names = nltk.corpus.names.words('female.txt')
-print(male_names[:5])
 
-labeled_names = [(name, 'male') for name in male_names ]
-labeled_names = [(name, 'female') for name in female_names ]
+#concatenate 2 nupmy array
+np_list = np.concatenate((np.array(male_names), np.array(female_names)))
 
-random.seed(12347)
-random.shuffle(labeled_names)
-print(labeled_names[:5])
-
-def get_features_names(name):
-    return {
-        'first_letter':name[0],
-        'last_but_one_letter':name[-2],
-        'last_letter':name[-1],
-        'length':len(name),
-    }
+#create dataFrame -> direction
+df_male = pd.DataFrame({'name' : np.array(male_names), 'class' : 1})
+df_female = pd.DataFrame({'name' : np.array(female_names), 'class' : 0})
 
 
-features = [(get_features_names(name), gender) for (name,gender) in labeled_names]
+#concatenate 2 data frame
+df = pd.concat([df_male, df_female], axis=0)
 
-train_set, test_set = features[1000:], features[:1000]
+#create feature (first letter, last letter, len )
+df['first'] = df.name.apply(lambda x: x[0])
+df['last'] = df.name.apply(lambda x: x[-1])
+df['len'] = df.name.apply(lambda x: len(x))
 
 
-bayes_classifier = nltk.NaiveBayesClassifier.train(train_set) #wytrenowanie klasyfikatora na zbiorze uczącym
+print(df.head())
 
-print(bayes_classifier.classify(get_features_names('Alice'))) #sprawdzenie poprawności rozpoznawania płci
+print(df.nunique())
 
-print(nltk.classify.accuracy(bayes_classifier, test_set)) # dpkładność klasyfiakra
+#create train data frame
+df_train = df.copy()
+y = df_train['class'].values
+df_train = df_train.drop(['name', 'class'], axis=1)
+df_train = pd.get_dummies(df_train, drop_first=True)
 
-bayes_classifier.show_most_informative_features(10)
+X = df_train.values
+
+#print(df_train.shape)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=29, test_size=0.2)
+
+clf = RandomForestClassifier()
+
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+#print(confusion_matrix(y_test, y_pred))
+
+ef=(y_pred == y_test).sum()/y_pred.shape[0]
+print('effectiveness:', ef)
+
+
+wyn = pd.DataFrame({'value': clf.feature_importances_, 'name': df_train.columns})
+print(wyn.head())
+
+x=wyn['name']
+y=wyn['value']
+plt.bar(x,y, align='center')
+#or scatter plot
+#plt.scatter(x,y)
+plt.xticks(rotation=60)
+plt.legend(loc='upper right')
+
+
+
+
+plt.show()
