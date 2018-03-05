55f3d0e4f757dc74b0fc9b492725d518a8355a76
Agulaa <agnieszkaxrusin@gmail.com>
2018-03-05 22:52:58 +0100

    Adding new functions

--- chatbot.py
+++ chatbot.py
@@ -19,6 +19,54 @@ responses = {
     "what's the weather today?": ["it's sunny!"], 'I like you.': ['Thank you']
 }
 
+keywords = {
+    'greet': ['hello', 'hi', 'hey'],
+    'thankyou': ['thank', 'thx'],
+    'goodbay': ['bye', 'farewell']
+}
+key_responses = {
+    'default': 'default message',
+    'goodbye': 'goodbye for now',
+    'greet': 'Hello you! :)',
+    'thankyou': 'you are very welcome'
+}
+
+
+# Define a dictionary of patterns
+patterns = {}
+def define_dictionary():
+
+    # Iterate over the keywords dictionary
+    for intent, keys in keywords.items():
+        # Create regular expressions and compile them into pattern objects
+        patterns[intent] = re.compile('|'.join(keys))
+    return patterns
+
+
+patterns = define_dictionary()
+
+
+# Define a function to find the intent of a message
+def match_intent(message):
+    matched_intent = None
+
+    for intent, pattern in patterns.items():
+        # Check if the pattern occurs in the message
+        if pattern.search(message):
+            matched_intent = intent
+    return matched_intent
+
+
+# Define a respond with key function
+def respond_key(message):
+    # Call the match_intent function
+    intent = match_intent(message)
+    # Fall back to the default response
+    key = "default"
+    if intent in key_responses:
+        key = intent
+    return key_responses[key]
+
 
 def send_message_to_bot():
     message = input('What do you want to send to bot ?')
@@ -44,7 +92,7 @@ def send_message(message):
     # Print user_template including the user_message
     print(user_template.format(message))
     # Get the bot's response to the message
-    response = respond(message)
+    response = respond_key(message)
     # Print the bot template including the bot's response.
     print(bot_template.format(response))
 
@@ -69,6 +117,32 @@ def replace_pronouns(message):
     return message
 
 
+# Define find_name()
+def find_name(message):
+    name = None
+    # Create a pattern for checking if the keywords occur
+    name_keyword = re.compile(r"\b(name|call)\b")
+    # Create a pattern for finding capitalized words
+    name_pattern = re.compile('[A-Z]{1}[a-z]*')
+    if name_keyword.search(message):
+        # Get the matching words in the string
+        name_words = name_pattern.findall(message)
+        if len(name_words) > 0:
+            # Return the name if the keywords are present
+            name = ' '.join(name_words)
+    return name
+
+
+# Define respond()
+def respond(message):
+    # Find the name
+    name = find_name(message)
+    if name is None:
+        return "Hi there!"
+    else:
+        return "Hello, {0}!".format(name)
+
+
 def check_replace_pro():
     print(replace_pronouns("my last birthday"))
     print(replace_pronouns("when you went to Florida"))
@@ -77,6 +151,7 @@ def check_replace_pro():
 
 def main():
     # Send a message to the bot
+
     resp = True
     while resp:
         message = send_message_to_bot()
