b44c3ec3d3aa4ad249bfcef02ee883e1c45ca97b
Agulaa <agnieszkaxrusin@gmail.com>
2018-03-27 12:35:09 +0200

    Update

--- Decision Tree/Tree_entropy_iris.py
+++ Decision Tree/Tree_entropy_iris.py
@@ -7,64 +7,12 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 
-def plot_decision_regions_(X, y, classifier, test_idx=None, resolution=0.02):
-    """small convenience function to visualize the decision boundaries for 2D datasets"""
-
-    # setup marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    # create a color map from the list of colors via ListedColormap
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    # plot the decision surface
-    # determine the minimum and maximum values for the two feature
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    # create a pair of grid arrays use feature vectors
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    # trained our perceptron classifier on two feature dimensions
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    # reshaping the predicted class labels Z into a grid with the same dimensions as xx1 and xx2
-    Z = Z.reshape(xx1.shape)
-    #  draw a contour plot
-    #  function that maps the different decision regions to different colors for each predicted class in the grid array
-    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    # plot class samples
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0],
-                    y=X[y == cl, 1],
-                    alpha=0.8,
-                    c=colors[idx],
-                    marker=markers[idx],
-                    label=cl,
-                    edgecolor='black')
-
-    # highlight test samples
-    if test_idx:
-        # plot all samples
-        X_test, y_test = X[test_idx, :], y[test_idx]
-
-        plt.scatter(X_test[:, 0],
-                    X_test[:, 1],
-                    c='',
-                    edgecolor='black',
-                    alpha=1.0,
-                    linewidth=1,
-                    marker='o',
-                    s=100,
-                    label='test set')
-
 def train_test_datasets():
 
     iris = datasets.load_iris()
     X = iris.data[:, [2, 3]]
     y = iris.target
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3)
-
     tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
     tree.fit(X_train, y_train)
     return X_train, X_test, y_train, y_test, tree
@@ -72,18 +20,7 @@ def train_test_datasets():
 
 def visualization():
     X_train, X_test, y_train, y_test, tree = train_test_datasets()
-    X_combined = np.vstack((X_train, X_test))
-    y_combined = np.hstack((y_train, y_test))
 
-    # 1 row, 2 columns
-    plt.subplot(121)
-    plot_decision_regions_(X_combined, y_combined, classifier=tree, test_idx=range(105,150))
-    plt.title('With test set')
-    plt.xlabel('petal length [cm]')
-    plt.ylabel('petal width [cm]')
-    plt.legend(loc='upper left')
-
-    plt.subplot(122)
     plot_decision_regions(X_train, y_train, clf=tree, legend=2)
     plt.title('Without test set')
     plt.xlabel('petal length [cm]')
